29817a8f7a7ffe0a14332ab02162e7f697cb75d6
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2019-12-27 14:39:18 +0900

    introduce Photo model

--- src/Model/Model.elm
+++ src/Model/Model.elm
@@ -1,10 +1,19 @@
 module Model.Model exposing (Model, init)
 
 
+baseUrl : String
+baseUrl =
+    "https://programming-elm.com/"
+
+
 type alias Model =
-    {}
+    { url : String
+    , caption : String
+    }
 
 
 init : Model
 init =
-    {}
+    { url = baseUrl ++ "1.jpg"
+    , caption = "Surfing"
+    }
--- src/View/View.elm
+++ src/View/View.elm
@@ -6,16 +6,6 @@ import Model.Model exposing (Model)
 import Msg.Message exposing (Msg)
 
 
-baseUrl : String
-baseUrl =
-    "https://programming-elm.com/"
-
-
-url : String -> String
-url path =
-    baseUrl ++ path
-
-
 view : Model -> Html Msg
 view model =
     div []
@@ -24,17 +14,15 @@ view model =
             ]
         , div
             [ class "content-flow" ]
-            [ (detailedPhoto << url) "1.jpg" "Surfing"
-            , (detailedPhoto << url) "2.jpg" "The Fox"
-            , (detailedPhoto << url) "3.jpg" "Evening"
+            [ detailedPhoto model
             ]
         ]
 
 
-detailedPhoto : String -> String -> Html Msg
-detailedPhoto fullUrl caption =
+detailedPhoto : Model -> Html Msg
+detailedPhoto model =
     div [ class "detailed-photo" ]
-        [ img [ src fullUrl ] []
+        [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ h2 [ class "caption" ] [ text caption ] ]
+            [ h2 [ class "caption" ] [ text model.caption ] ]
         ]
