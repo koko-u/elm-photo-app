e4539f2cb1e5700b2531bb4a9a2c4f4f4dad5457
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2019-12-31 10:33:36 +0900

    extract love button parts from detailedPhoto

--- src/View/View.elm
+++ src/View/View.elm
@@ -22,6 +22,17 @@ view model =
 
 detailedPhoto : Model -> Html Msg
 detailedPhoto model =
+    div [ class "detailed-photo" ]
+        [ img [ src model.url ] []
+        , div [ class "photo-info" ]
+            [ loveButton model
+            , h2 [ class "caption" ] [ text model.caption ]
+            ]
+        ]
+
+
+loveButton : Model -> Html Msg
+loveButton model =
     let
         buttonClass =
             if model.liked then
@@ -30,14 +41,8 @@ detailedPhoto model =
             else
                 "off"
     in
-    div [ class "detailed-photo" ]
-        [ img [ src model.url ] []
-        , div [ class "photo-info" ]
-            [ div [ class "like-button", onClick ToggleLike ]
-                [ span [ class buttonClass ]
-                    [ i [ class "fas fa-heart fa-2x" ] []
-                    ]
-                ]
-            , h2 [ class "caption" ] [ text model.caption ]
+    div [ class "like-button", onClick ToggleLike ]
+        [ span [ class buttonClass ]
+            [ i [ class "fas fa-heart fa-2x" ] []
             ]
         ]
